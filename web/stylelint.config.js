7ed431052bd8bfcc6669552bece018e63fe38ecc
shinku <shinku_1314@qq.com>
2026-08-05 21:25:21 +0800

    [refactor] clean up web styles

--- web/stylelint.config.js
+++ web/stylelint.config.js
@@ -2,17 +2,12 @@ export default {
   extends: ["stylelint-config-standard"],
   ignoreFiles: ["dist/**/*", "node_modules/**/*", "coverage/**/*"],
   rules: {
-    "alpha-value-notation": null,
-    "color-hex-length": null,
     "color-function-notation": "modern",
     "custom-property-empty-line-before": null,
     "declaration-empty-line-before": null,
     "media-feature-range-notation": null,
     "no-descending-specificity": null,
-    "no-duplicate-selectors": null,
-    "property-no-deprecated": null,
     "rule-empty-line-before": null,
     "selector-class-pattern": null,
-    "value-keyword-case": null,
   },
 };
